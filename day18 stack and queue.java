94240a6cb3241de9537786736823b4d26afbe349
Nitisha Srivastav <nitishasrvstv@gmail.com>
2018-08-06 12:42:12 +0530

    Create day18 stack and queue.java

---
+++ day18 stack and queue.java
@@ -0,0 +1,14 @@
+Input Format
+
+You do not need to read anything from stdin. The locked stub code in your editor reads a single line containing string S .
+It then calls the methods specified above to pass each character to your instance variables.
+
+Constraints
+
+S is composed of lowercase English letters.
+Output Format
+
+You are not responsible for printing any output to stdout. 
+If your code is correctly written and S is a palindrome, the locked stub code will print 
+palindrome or not palindrome.
+Solution-
