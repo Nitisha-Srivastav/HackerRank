758588c17b6a45f32b59ff2009d1c4ce42fcc513
Nitisha Srivastav <nitishasrvstv@gmail.com>
2018-08-07 09:18:45 +0530

    Update day20 sorting.java

--- day20 sorting.java
+++ day20 sorting.java
@@ -12,3 +12,38 @@ where firstElement is the first element in the sorted array.
 Last Element: lastElement 
 where lastElement is the last element in the sorted array.
 Solution-
+  import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] a = new int[n];
+        int numSwaps=0,firstElement=0,lastElement=0,temp;
+        for(int a_i=0; a_i < n; a_i++){
+            a[a_i] = in.nextInt();
+        }
+        for(int i=0;i<a.length;i++){
+        for(int j=0;j<a.length-1;j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+                numSwaps++;
+            }
+           
+        }}
+         firstElement=a[0];
+        lastElement=a[a.length-1];
+        System.out.println("Array is sorted in "+numSwaps+" swaps.");
+        System.out.println("First Element: "+firstElement);
+        System.out.println("Last Element: "+lastElement);
+    }
+}
