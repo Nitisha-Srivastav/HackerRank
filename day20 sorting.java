ccd039457da7e381ff10a45115b2e3fe9740ee17
Nitisha Srivastav <nitishasrvstv@gmail.com>
2018-08-07 08:47:46 +0530

    Create day20 sorting.java

---
+++ day20 sorting.java
@@ -0,0 +1,14 @@
+Input Format
+
+The first line contains an integer,n , denoting the number of elements in array a. 
+The second line contains n space-separated integers describing the respective values of a0,a1,....an-1 .
+
+Print the following three lines of output:
+
+Array is sorted in numSwaps swaps. 
+where numSwaps is the number of swaps that took place.
+First Element: firstElement 
+where firstElement is the first element in the sorted array.
+Last Element: lastElement 
+where lastElement is the last element in the sorted array.
+Solution-
