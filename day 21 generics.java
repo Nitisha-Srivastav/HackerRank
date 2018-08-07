f98c8634c31f3ee720766da87a436c99846b78b0
Nitisha Srivastav <nitishasrvstv@gmail.com>
2018-08-07 09:51:50 +0530

    Create day 21 generics.java

---
+++ day 21 generics.java
@@ -0,0 +1,11 @@
+Input Format
+
+The locked Solution class in your editor will pass different types of arrays to your printArray function.
+
+Constraints
+
+You must have exactly 1 function named printArray.
+Output Format
+
+Your printArray function should print each element of its generic array parameter on a new line.
+Solution-
