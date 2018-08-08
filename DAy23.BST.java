50b08a477410f5a3b5e3d660c659c172c30f6bbd
Nitisha Srivastav <nitishasrvstv@gmail.com>
2018-08-08 11:44:59 +0530

    Create DAy23.BST.java

---
+++ DAy23.BST.java
@@ -0,0 +1,10 @@
+Input Format
+
+The locked stub code in your editor reads the following inputs and assembles them into a BST: 
+The first line contains an integer,T  (the number of test cases). 
+The T subsequent lines each contain an integer,data , denoting the value of an element that must be added to the BST.
+
+Output Format
+
+Print the data value of each node in the tree's level-order traversal as a single line of N space-separated integers.
+Solution
