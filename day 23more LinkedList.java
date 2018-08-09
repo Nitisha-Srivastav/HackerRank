75f21094d25909760af2df6892d1c00dacf244c8
Nitisha Srivastav <nitishasrvstv@gmail.com>
2018-08-09 09:59:18 +0530

    Create day 23more LinkedList.java

---
+++ day 23more LinkedList.java
@@ -0,0 +1,16 @@
+Input Format
+
+You do not need to read any input from stdin. 
+The following input is handled by the locked stub code and passed to the removeDuplicates function: 
+The first line contains an integer,N , the number of nodes to be inserted. 
+The N subsequent lines each contain an integer describing the data value of a node being inserted at the list's tail.
+
+Constraints
+
+The data elements of the linked list argument will always be in non-decreasing order.
+
+Output Format
+
+Your removeDuplicates function should return the head of the updated linked list.
+The locked stub code in your editor will print the returned list to stdout.
+Solution-
